9420b0737d43d76631419fc8905f0f83bee727c4
Евгений Швецов <67862313+EvgenyShvetcov@users.noreply.github.com>
2023-08-07 02:07:53 +0300

    fix

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,6 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { rootReducer } from "./redux";
-import createSagaMiddleWare from "redux-saga";
-import rootSaga from "./sagas";
 import { allApi } from "./services/Services";
-
-// const sagaMiddleware = createSagaMiddleWare();
 
 export const store = configureStore({
   reducer: rootReducer,
@@ -13,16 +9,3 @@ export const store = configureStore({
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-
-// sagaMiddleware.run(rootSaga);
-
-// export const store = configureStore({
-//   reducer: rootReducer,
-//   middleware: (getDefaultMiddleware) => {
-//     getDefaultMiddleware().concat(chapterApi.middleware);
-//   },
-// });
-
-// export type AppDispatch = typeof store.dispatch;
-// export type RootState = ReturnType<typeof store.getState>;
-// setupListeners(store.dispatch);
