17b933fd673511cccdfacd0b796a4317d958e578
Евгений Швецов <67862313+EvgenyShvetcov@users.noreply.github.com>
2023-08-10 01:18:24 +0300

    server client separated

---
+++ client/src/components/CommentSection/CommentSection.tsx
@@ -0,0 +1,53 @@
+import { FC, useState } from "react";
+import "./CommentSection.scss";
+import { PostState, UserState } from "../../interfaces";
+import { CommentComponent } from "../CommentComponent/CommentComponent";
+import { Button, Input } from "@mui/material";
+import { allApi } from "../../store/services/Services";
+
+interface Props {
+  data: PostState | undefined;
+  User: UserState | null;
+}
+
+export const CommentSection: FC<Props> = ({ data, User }) => {
+  console.log(data?.comments);
+  const [createComment, commentFetchData] =
+    allApi.useFetchCreateCommentMutation();
+  const [currentComment, setCurrentComment] = useState<string>("");
+  return (
+    <div className="comments">
+      {data && data.comments.length !== 0
+        ? data.comments.map((el) => (
+            <CommentComponent
+              text={el.text}
+              User={el.user?.fullName || ""}
+              date={el.date}
+            />
+          ))
+        : "Комментариев пока нет."}
+      {User && (
+        <div className="addCommentSection">
+          <Input
+            fullWidth
+            value={currentComment}
+            onChange={(e) => setCurrentComment(e.target.value)}
+          />
+          <Button
+            className="addButton"
+            onClick={() => {
+              createComment({
+                text: currentComment,
+                Author: User?._id || "",
+                post: data?._id || "",
+              });
+              setCurrentComment("");
+            }}
+          >
+            Прокомментировать
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+};
